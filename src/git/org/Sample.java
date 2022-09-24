83a7204b8ba39d898f1869501ae9886b31be024f
venkatjalu <venkatjalu122@gmail.com>
2022-09-24 15:01:14 +0530

    add mmmm

--- src/git/org/Sample.java
+++ src/git/org/Sample.java
@@ -2,17 +2,10 @@ package git.org;
 
 public class Sample {
 public static void main(String[] args) {
-
+  
 	System.out.println("Hi");
-	System.out.println("bro");
-	System.out.println("How r u");
-	System.err.println("hi hi");
-    System.out.println("abc");
-	System.out.println("Hi");
-	System.out.println("bro");
-	System.out.println("How r u");
-	System.err.println("hi hi");
-    System.out.println("abc");
 
+System.out.println("vanakkam");
+System.out.println("abcd");
 }
 }
