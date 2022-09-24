4bea0d50acc61c5a1860c4b3e56b1dc2498c660d
venkatjalu <venkatjalu122@gmail.com>
2022-09-24 14:46:27 +0530

    new add

--- src/git/org/Sample.java
+++ src/git/org/Sample.java
@@ -7,7 +7,12 @@ public static void main(String[] args) {
 	System.out.println("bro");
 	System.out.println("How r u");
 	System.err.println("hi hi");
-System.out.println("abc");
+    System.out.println("abc");
+	System.out.println("Hi");
+	System.out.println("bro");
+	System.out.println("How r u");
+	System.err.println("hi hi");
+    System.out.println("abc");
 
 }
 }
